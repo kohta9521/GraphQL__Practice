bed4f5ba82bc67ac9137979f15ef032156b48c69
k_95.21 <kohta0421kochi@gmail.com>
2024-01-18 14:25:59 +0900

    add new test key

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,35 @@
-const { AppoloServer, gql } = require('apollo-server-express');
+const { AppoloServer, gql } = require("apollo-server-express");
+
+const books = [
+  {
+    title: "吾輩は猫である",
+    author: "夏目漱石",
+  },
+  {
+    title: "走れメロス",
+    author: "太宰治",
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String,
+    author: String
+  }
+
+  typeQuery {
+    test: [book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    test: () => books,
+  },
+};
 
 const server = new AppoloServer({});
 
-server.listen().then((( url )) => {
-    console.log(`Server ready at ${url}`)
-})
+server.listen().then((url) => {
+  console.log(`Server ready at ${url}`);
+});
