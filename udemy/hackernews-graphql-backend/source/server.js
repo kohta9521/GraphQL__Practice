4ef7e53e4fd9ea9beef124f04fabd2ba84040bfb
k_95.21 <kohta0421kochi@gmail.com>
2024-09-25 18:31:38 +0900

    complete new server running and new resolvers made

--- udemy/hackernews-graphql-backend/source/server.js
+++ udemy/hackernews-graphql-backend/source/server.js
@@ -6,3 +6,17 @@ const typeDefs = gql`
     info: String!
   }
 `;
+
+// Resolvers for the schema fields
+const resolvers = {
+  Query: {
+    info: () => "Hacker News Clone",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`${url} is running`));
