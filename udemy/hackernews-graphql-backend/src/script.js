ed6b7e6040b6320629016ffb012a121f4fee7734
k_95.21 <kohta0421kochi@gmail.com>
2024-09-28 20:55:37 +0900

    add new todoapp graphql files

---
+++ udemy/hackernews-graphql-backend/src/script.js
@@ -0,0 +1,24 @@
+// データベースにアクセスするためのクライアントライブラリ
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "GraphQL チュートリアルをUdemyで学ぶ",
+      url: "https://www.udemy.com/course/graphql-bootcamp/",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    // データベース接続を閉じる
+    prisma.$disconnect();
+  });
